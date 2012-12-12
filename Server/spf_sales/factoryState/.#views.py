2eb74df2052592177b4bc7101f8a8762da475dc3
japhy <japhy@li414-198.members.linode.com>
2012-12-12 09:15:11 -0500

    getting everything into git blurgh

---
+++ Server/spf_sales/factoryState/.#views.py
@@ -0,0 +1,1 @@
+japhy@li414-198.members.linode.com.11889:1343815918
--- Server/spf_sales/factoryState/models.py
+++ Server/spf_sales/factoryState/models.py
@@ -3,14 +3,51 @@ from django.forms import ModelForm
 
 class FactoryState(models.Model):
     #mapping
-    _1ma
-    _2ma
-    _3ma
-    command  =   models.CharField(max_length=20)
-    commandTimeStamp    =   models.DateTimeField(auto_now=True)
-    parameter=models.FloatField()
-    status      =   models.CharField(max_length=20)
-    statusTimeStamp   =   models.DateTimeField(auto_now=True)
-    description = models.TextField()
-    json = models.TextField()
+    _1ma=models.IntegerField()
+    _2ma=models.IntegerField()
+    _3ma=models.IntegerField()
+    _4ma=models.IntegerField()
 
+    #axis settings
+    
+    #feed rate
+    _xfr=models.IntegerField()
+    _yfr=models.IntegerField()
+    _zfr=models.IntegerField()
+    _afr=models.IntegerField()
+
+    #max velocity
+    _xvm=models.IntegerField()
+    _yvm=models.IntegerField()
+    _zvm=models.IntegerField()
+    _avm=models.IntegerField()
+
+    #travel
+    _xtr=models.FloatField()
+    _ytr=models.FloatField()
+    _ztr=models.FloatField()
+    _atr=models.FloatField()
+
+    #step angle
+    _xsa=models.FloatField()
+    _ysa=models.FloatField()
+    _zsa=models.FloatField()
+    _asa=models.FloatField()
+
+    #power mode
+    _1pm=models.IntegerField()
+    _2pm=models.IntegerField()
+    _3pm=models.IntegerField()
+    _4pm=models.IntegerField()
+    
+    #raw materials in stock
+    solettesInHopper=models.IntegerField()  #number of solettes
+    tabbingOnSpool=models.IntegerField()  #mm of tabbing
+    backingsInHopper=models.IntegerField()  #number of backings
+    encapsulantInTub=models.IntegerField()  #ccs of encapsulant
+
+    backingWidth=models.FloatField()  #in mm
+    backingLength=models.FloatField() #in mm
+
+    soletteWidth=models.FloatField()  #in mm
+    soletteLength=models.FloatField()  # in mm
